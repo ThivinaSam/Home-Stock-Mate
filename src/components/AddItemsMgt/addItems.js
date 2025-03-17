72b85cd9e6acafda69f0692e7f9b2944e893ff89
ThivinaSam <152647603+ThivinaSam@users.noreply.github.com>
2025-03-17 13:39:27 +0530

    created 4 folders for 4 functions

---
+++ src/components/AddItemsMgt/addItems.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function addItems() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default addItems
---
+++ src/components/FinanceMgt/finance.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function finance() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default finance
