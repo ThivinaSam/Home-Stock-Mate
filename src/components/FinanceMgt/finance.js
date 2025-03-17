b5da93c030c17d1944ab47b0bb3d44c18421ea50
ThivinaSam <152647603+ThivinaSam@users.noreply.github.com>
2025-03-17 14:03:35 +0530

    implemented and added routes to function pages

--- src/components/FinanceMgt/finance.js
+++ src/components/FinanceMgt/finance.js
@@ -3,7 +3,7 @@ import React from 'react'
 function finance() {
   return (
     <div>
-      
+      <h2>Finance Management</h2>
     </div>
   )
 }
--- src/components/GetItemsMgt/getItems.js
+++ src/components/GetItemsMgt/getItems.js
@@ -3,7 +3,7 @@ import React from 'react'
 function getItems() {
   return (
     <div>
-      
+      <h2>Get Items</h2>
     </div>
   )
 }
