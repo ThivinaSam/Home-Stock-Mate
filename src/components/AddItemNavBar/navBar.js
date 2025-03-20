56e052f5504e9262805c5547b52f5f95e9707265
ThivinaSam <152647603+ThivinaSam@users.noreply.github.com>
2025-03-20 15:23:40 +0530

    changed add edit item form to popup

--- src/components/AddItemNavBar/navBar.js
+++ src/components/AddItemNavBar/navBar.js
@@ -1,41 +1,46 @@
-import React from "react";
-import { Menu, Container, Button, Image } from "semantic-ui-react";
-import { useNavigate, Link } from "react-router-dom";
-import logo from "../../logo.svg";
+import React, { useState } from "react";
+import { Menu, Header, Button, Container } from "semantic-ui-react";
+import { useNavigate } from "react-router-dom";
+import AddItemModal from "../AddItemsMgt/AddEditItemModal";
 
-const NavBar = () => {
+const NavBar = ({ refreshItems }) => {
+  const [open, setOpen] = useState(false);
   const navigate = useNavigate();
 
   return (
-    <div>
-      <Menu
-        inverted
-        borderless
-        style={{
-          padding: "0",
-          marginBottom: "20px",
-          minHeight: "40px" /* Reduce the minimum height */,
-          height: "80px",
-        }}
-        attached
-      >
-        <Container>
-          <Menu.Item name="home">
-            <Link to="/">
-              <Image src={logo} size="tiny" alt="logo" />
-            </Link>
+    <>
+      <Menu size="large" inverted style={{ margin: 0 }}>
+        {/* Use a customized Container with less padding */}
+        <Container style={{ width: '80%', maxWidth: '1400px', padding: '0 10px' }}>
+          <Menu.Item style={{ paddingLeft: '0' }}>
+            <Header as="h3" inverted style={{ marginRight: '5px' }}>
+              Home Stock Mate
+            </Header>
           </Menu.Item>
-          <Menu.Item>
-            <h2 className="text-2xl">React Firebase CRUD with Upload Image</h2>
-          </Menu.Item>
-          <Menu.Item position="right">
-            <Button size="mini" primary onClick={() => navigate("/addItem")}>
-              Add Item
-            </Button>
-          </Menu.Item>
+
+          <Menu.Item name="Home" onClick={() => navigate("/addItemHome")} />
+
+          {/* Spacer item - takes up available space */}
+          <Menu.Item style={{ flex: 1 }}></Menu.Item>
+
+          {/* Right-aligned items in correct order */}
+          <Menu.Menu position="right">
+            <Menu.Item onClick={() => navigate("/")}>
+              Go to Dashboard
+            </Menu.Item>
+            
+            <Menu.Item style={{ paddingRight: '0' }}>
+              <Button primary onClick={() => setOpen(true)}>
+                Add Item
+              </Button>
+            </Menu.Item>
+          </Menu.Menu>
         </Container>
       </Menu>
-    </div>
+
+      {/* Add the modal component */}
+      <AddItemModal open={open} setOpen={setOpen} refreshItems={refreshItems} />
+    </>
   );
 };
 
--- src/components/AddItemsMgt/addItemsHome.js
+++ src/components/AddItemsMgt/addItemsHome.js
@@ -10,15 +10,26 @@ import {
   Loader,
 } from "semantic-ui-react";
 import { useNavigate } from "react-router-dom";
+import NavBar from "../AddItemNavBar/navBar";
 import { collection, deleteDoc, onSnapshot, doc } from "firebase/firestore";
 import { ref, deleteObject } from "firebase/storage";
 import ModelComp from "./modelComp";
+import AddItemModal from "./AddEditItemModal"; // Import the AddItemModal component
 
 const AddItemsHome = () => {
   const [items, setItems] = useState([]);
   const [open, setOpen] = useState(false);
   const [item, setItem] = useState({});
   const [loading, setLoading] = useState(false);
+  const [addItemModalOpen, setAddItemModalOpen] = useState(false); // State for AddItemModal
+  const [selectedItemId, setSelectedItemId] = useState(null); // State for selected item ID
+
+  // Function to refresh items list
+  const refreshItems = () => {
+    setLoading(true);
+    // You can re-fetch data if needed, but the onSnapshot should handle updates automatically
+  };
+
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -49,33 +60,35 @@ const AddItemsHome = () => {
   };
 
   const handleDelete = async (id) => {
-    if(window.confirm("Are you sure you want to delete this item?")) {
+    if (window.confirm("Are you sure you want to delete this item?")) {
       try {
         // Find the item to get its image URL
-        const itemToDelete = items.find(item => item.id === id);
-        
+        const itemToDelete = items.find((item) => item.id === id);
+
         // Close the modal
         setOpen(false);
-        
+
         // Delete the Firestore document
         await deleteDoc(doc(db, "addItems", id));
-        
+
         // If the item has an image, delete it from storage
-        if(itemToDelete && itemToDelete.image) {
+        if (itemToDelete && itemToDelete.image) {
           // Extract the path from the full URL
           // Firebase Storage URLs typically contain a path like "gs://bucket-name/path/to/file.jpg"
           // or https://firebasestorage.googleapis.com/v0/b/bucket-name/o/path%2Fto%2Ffile.jpg
-          
+
           try {
             // For a URL like https://firebasestorage.googleapis.com/...
             const imageUrl = new URL(itemToDelete.image);
-            const pathWithQuery = imageUrl.pathname.split('/o/')[1];
-            const imagePath = pathWithQuery ? decodeURIComponent(pathWithQuery.split('?')[0]) : null;
-            
-            if(imagePath) {
+            const pathWithQuery = imageUrl.pathname.split("/o/")[1];
+            const imagePath = pathWithQuery
+              ? decodeURIComponent(pathWithQuery.split("?")[0])
+              : null;
+
+            if (imagePath) {
               // Create a reference to the file
               const imageRef = ref(storage, imagePath);
-              
+
               // Delete the file
               await deleteObject(imageRef);
               console.log("Image deleted successfully");
@@ -85,7 +98,7 @@ const AddItemsHome = () => {
             // Continue with document deletion even if image deletion fails
           }
         }
-        
+
         // Update the UI by filtering out the deleted item
         setItems(items.filter((item) => item.id !== id));
       } catch (error) {
@@ -95,72 +108,81 @@ const AddItemsHome = () => {
   };
 
   return (
-    <Container>
-      {loading ? (
-        <Loader active inline="centered" />
-      ) : (
-        <Table celled>
-          <Table.Header>
-            <Table.Row>
-              <Table.HeaderCell>Image</Table.HeaderCell>
-              <Table.HeaderCell>Name</Table.HeaderCell>
-              <Table.HeaderCell>Description</Table.HeaderCell>
-              <Table.HeaderCell>Date</Table.HeaderCell>
-              <Table.HeaderCell>Quantity</Table.HeaderCell>
-              <Table.HeaderCell>Price</Table.HeaderCell>
-              <Table.HeaderCell>Actions</Table.HeaderCell>
-            </Table.Row>
-          </Table.Header>
+    <div>
+      <NavBar refreshItems={refreshItems} />
+      <Container style={{ marginTop: "20px" }}>
+        {loading ? (
+          <Loader active inline="centered" />
+        ) : (
+          <Table celled>
+            <Table.Header>
+              <Table.Row>
+                <Table.HeaderCell>Image</Table.HeaderCell>
+                <Table.HeaderCell>Name</Table.HeaderCell>
+                <Table.HeaderCell>Description</Table.HeaderCell>
+                <Table.HeaderCell>Date</Table.HeaderCell>
+                <Table.HeaderCell>Quantity</Table.HeaderCell>
+                <Table.HeaderCell>Price</Table.HeaderCell>
+                <Table.HeaderCell>Actions</Table.HeaderCell>
+              </Table.Row>
+            </Table.Header>
 
-          <Table.Body>
-            {items &&
-              items.map((item) => (
-                <Table.Row key={item.id}>
-                  <Table.Cell>
-                    <Image
-                      src={
-                        item.image ||
-                        "https://react.semantic-ui.com/images/wireframe/image.png"
-                      }
-                      size="tiny"
-                      circular
-                      style={{ width: "50px", height: "50px" }}
-                    />
-                  </Table.Cell>
-                  <Table.Cell>{item.item}</Table.Cell>
-                  <Table.Cell>{item.description}</Table.Cell>
-                  <Table.Cell>{item.date}</Table.Cell>
-                  <Table.Cell>{item.quantity}</Table.Cell>
-                  <Table.Cell>{item.price}</Table.Cell>
-                  <Table.Cell>
-                    <Button
-                      color="green"
-                      onClick={() => navigate(`/updateItem/${item.id}`)}
-                    >
-                      Update
-                    </Button>
-                    <Button color="purple" onClick={() => handleModel(item)}>
-                      View
-                    </Button>
-                    {/* {open && (
-                      <ModelComp
-                        open={open}
-                        setOpen={setOpen}
-                        handleDelete={() => console.log("delete")}
-                        {...item}
+            <Table.Body>
+              {items &&
+                items.map((item) => (
+                  <Table.Row key={item.id}>
+                    <Table.Cell>
+                      <Image
+                        src={
+                          item.image ||
+                          "https://react.semantic-ui.com/images/wireframe/image.png"
+                        }
+                        size="tiny"
+                        circular
+                        style={{ width: "50px", height: "50px" }}
                       />
-                    )} */}
-                  </Table.Cell>
-                </Table.Row>
-              ))}
-          </Table.Body>
-        </Table>
-      )}
+                    </Table.Cell>
+                    <Table.Cell>{item.item}</Table.Cell>
+                    <Table.Cell>{item.description}</Table.Cell>
+                    <Table.Cell>{item.date}</Table.Cell>
+                    <Table.Cell>{item.quantity}</Table.Cell>
+                    <Table.Cell>{item.price}</Table.Cell>
+                    <Table.Cell>
+                      <Button
+                        color="green"
+                        onClick={() => {
+                          setSelectedItemId(item.id);
+                          setAddItemModalOpen(true);
+                        }}
+                      >
+                        Edit
+                      </Button>
+                      <Button color="purple" onClick={() => handleModel(item)}>
+                        View
+                      </Button>
+                    </Table.Cell>
+                  </Table.Row>
+                ))}
+            </Table.Body>
+          </Table>
+        )}
 
-      <ModelComp open={open} setOpen={setOpen} item={item} handleDelete={handleDelete} />
+        <ModelComp
+          open={open}
+          setOpen={setOpen}
+          item={item}
+          handleDelete={handleDelete}
+        />
 
-      {/* Add the modal component outside of the map function */}
-      {/* <Modal open={open} onClose={() => setOpen(false)}>
+        <AddItemModal
+          open={addItemModalOpen}
+          setOpen={setAddItemModalOpen}
+          itemId={selectedItemId}
+          refreshItems={refreshItems}
+        />
+
+        {/* Add the modal component outside of the map function */}
+        {/* <Modal open={open} onClose={() => setOpen(false)}>
         <Modal.Header>Item Details</Modal.Header>
         <Modal.Content>
           <Modal.Description>
@@ -184,7 +206,8 @@ const AddItemsHome = () => {
           </Button>
         </Modal.Actions>
       </Modal> */}
-    </Container>
+      </Container>
+    </div>
   );
 };
 
